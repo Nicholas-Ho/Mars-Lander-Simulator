db23212f6ab390230f64f1da2c646e1be06c2ab5
Nicholas Ho <j.xiong@live.com>
2022-08-12 00:07:18 +0800

    Assignment 3 completed

--- Assignment 3/spring.cpp
+++ Assignment 3/spring.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() {
+void euler() {
 
   // declare variables
   double m, k, x, v, t_max, dt, t, a;
@@ -61,4 +61,77 @@ int main() {
   plt.show()
 
   */
+}
+
+void verlot() {
+  
+  // declare variables
+  double m, k, x, v, t_max, dt, t, a;
+  vector<double> t_list, x_list, v_list;
+
+  // mass, spring constant, initial position and velocity
+  m = 1;
+  k = 1;
+  x = 0;
+  v = 1;
+
+  // simulation time and timestep
+  t_max = 100;
+  dt = 0.1;
+
+  // initialise lists
+  x_list = {x, x + dt*v};
+  v_list = {v};
+
+  // Euler integration
+  for (t = dt; t <= t_max; t = t + dt) {
+
+    a = -k * x_list.back() / m; // current acceleration
+    x = 2 * x_list.back() - x_list.end()[-2] + (dt*dt) * a;
+    v = (x - x_list.end()[-2]) / (2 * dt);
+
+    // append current state to trajectories
+    t_list.push_back(t);
+    x_list.push_back(x);
+    v_list.push_back(v);
+
+    // calculate new position and velocity
+    a = -k * x / m;
+    x = x + dt * v;
+    v = v + dt * a;
+
+  }
+
+  x_list.pop_back();
+
+  // Write the trajectories to file
+  ofstream fout;
+  fout.open("trajectories.txt");
+  if (fout) { // file opened successfully
+    for (int i = 0; i < t_list.size(); i = i + 1) {
+      fout << t_list[i] << ' ' << x_list[i] << ' ' << v_list[i] << endl;
+    }
+  } else { // file did not open successfully
+    cout << "Could not open trajectory file for writing" << endl;
+  }
+
+  /* The file can be loaded and visualised in Python as follows:
+
+  import numpy as np
+  import matplotlib.pyplot as plt
+  results = np.loadtxt('trajectories.txt')
+  plt.figure(1)
+  plt.clf()
+  plt.xlabel('time (s)')
+  plt.grid()
+  plt.plot(results[:, 0], results[:, 1], label='x (m)')
+  plt.plot(results[:, 0], results[:, 2], label='v (m/s)')
+  plt.legend()
+  plt.show()
+
+  */
+} 
+
+void main() {
+  euler();
 }
